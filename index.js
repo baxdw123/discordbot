394bde59c88d109aad2c6aac2e9ce685201dc4db
baxdw123 <63054037+baxdw123@users.noreply.github.com>
2020-08-23 14:03:53 +0200

    init
    init

---
+++ index.js
@@ -0,0 +1,12 @@
+const discord = require("discord.js");
+const botConfig = require("./botconfig.json");
+
+const client = new discord.Client();
+client.login(process.env.token);
+
+client.on("ready", async () =>{
+
+console.log(`${client.user.username} is klaar voor gereet!!1!`);
+client.user.setActivity("Minecraft", { type: "PLAYING" });
+
+});
